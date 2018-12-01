93468cd84e30fe092f169ec23acd9244573490c2
Cheng Vue <cvu3@ucsc.edu>
2018-12-01 23:04:39 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,24 @@
 class Transfer
-  # your code here
+  attr_accessor :status, :sender, :receiver, :amount
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @status = "pending"
+    @receiver = receiver
+    @amount = amount
+  end
+
+  def valid?
+    @sender.valid? == true && @receiver.valid? == true
+  end
+
+  def execute_transaction
+    if self.valid? == true
+      @receiver.deposit(@amount)
+      @sender.balance -= @amount
+      self.status = "complete"
+    else
+      "Transaction rejected. Please check your account balance."
+    end
+  end
 end
